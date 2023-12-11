dd51c95d21cad0012ae6c2b02bd1802a21f562ce
yeudaborodyanski <yehudab@hadran.net>
2023-12-11 12:00:56 +0200

    db conf

--- src/pages/api/[...remult].ts
+++ src/pages/api/[...remult].ts
@@ -3,14 +3,13 @@ import {Procedure} from "@/model/Procedure";
 import {User} from "@/model/User";
 import {Event} from "@/model/Event";
 import {getUserOnServer} from "@/pages/api/auth/[[...nextauth]]";
+import {createPostgresDataProvider} from "remult/postgres";
 
 export default remultNext({
     entities: [
         Procedure, User, Event
     ],
     getUser: getUserOnServer,
-    // dataProvider:
-    //     createPostgresDataProvider({
-    //         connectionString: "---", // default: process.env["DATABASE_URL"]
-    //     })
+    dataProvider:
+        createPostgresDataProvider()
 })
