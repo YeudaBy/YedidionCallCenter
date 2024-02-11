c127c3bf0235e05ef963bed990fab5e8944d5e5f
yeudaborodyanski <yehudab@hadran.net>
2024-02-12 00:33:32 +0200

    delete files

--- src/pages/procedure/index.tsx
+++
@@ -1,8 +0,0 @@
-import {Procedure} from "../../model/Procedure";
-import {remult} from "remult";
-
-const procedureRepo = remult.repo(Procedure);
-
-export default function ProceduresPage() {
-    return <div>Procedure</div>
-}
