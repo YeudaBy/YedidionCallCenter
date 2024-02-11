83a86f6672c36576508e85c612d3cf3537658341
yeudaborodyanski <yehudab@hadran.net>
2024-02-12 00:54:01 +0200

    fix db

--- src/pages/api/[...remult].ts
+++ src/pages/api/[...remult].ts
@@ -2,6 +2,9 @@ import {remultNext} from 'remult/remult-next'
 import {Procedure} from "@/model/Procedure";
 import {User} from "@/model/User";
 import {getUserOnServer} from "./auth/[...nextauth]";
+import {createPostgresDataProvider} from "remult/postgres";
+import {JsonDataProvider} from "remult";
+import {JsonEntityFileStorage} from "remult/server";
 
 export const api = remultNext({
     entities: [
@@ -11,7 +14,20 @@ export const api = remultNext({
     //     UsersController
     // ],
     getUser: getUserOnServer,
-    logApiEndPoints: true
+    logApiEndPoints: true,
+
+    dataProvider:
+        production() ?
+            createPostgresDataProvider({
+                connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+            }) : async () => {
+                return new JsonDataProvider(new JsonEntityFileStorage("./db"))
+            },
 })
 
 export default api
+
+
+function production() {
+    return process.env.NODE_ENV !== 'development';
+}
