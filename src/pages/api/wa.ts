b1d4742e7e13a0b7be340cf2e4edee6314dbf754
yeudaborodyanski <yehudab@hadran.net>
2024-09-25 17:51:37 +0300

    null check

--- src/pages/api/wa.ts
+++ src/pages/api/wa.ts
@@ -5,11 +5,16 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     switch (req.method) {
         case 'POST': {
             const update = req.body as WaWebhook;
-            const message = update.entry[0].changes[0].value.messages[0];
-            if (!!message) {
-                console.log('Received message:', message.from, message.id, message.text);
+            try {
+                const message = update.entry[0].changes?.[0].value.messages?.[0];
+                if (!!message) {
+                    console.log('Received message:', message.from, message.id, message.text);
+                }
+                res.status(200).json({success: true});
+            } catch (e) {
+                console.error('Error processing webhook:', e);
+                res.status(200).json({success: false});
             }
-            res.status(200).json({success: true});
         }
             break
         case 'GET': {
