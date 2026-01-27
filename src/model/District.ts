d06aa5d27b0b145c70f36a903c662b7073a91e4d
yeudaborodyanski <yeudaborodyanski@gmail.com>
2026-01-27 21:24:26 +0200

    Revert "change env file in docker-compose.yml"
    This reverts commit c6ca19a5d802a3322f21e7bd306d465b7c761b56.

--- src/model/District.ts
+++ src/model/District.ts
@@ -2,7 +2,6 @@ export enum District {
     General = "ארצי",
     Jerusalem = "ירושלים",
     Central = "מרכז",
-    CentralSchooners = "מרכז - תיכונים",
     Northern = "צפון",
     Southern = "דרום",
 }
