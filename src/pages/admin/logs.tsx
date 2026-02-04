c4e4bcc9687019e04a26e8de238be52f8575fb0b
yeudaborodyanski <yeudaborodyanski@gmail.com>
2026-02-04 23:13:11 +0200

    add boilerplate to pass build

---
+++ src/pages/admin/logs.tsx
@@ -0,0 +1,8 @@
+export default function LogsPage() {
+    return (
+        <div>
+            <h1>Admin Logs Page</h1>
+            {/* Add your logs page implementation here */}
+        </div>
+    );
+}
