1ad8519600f1bc643219b860358bf62e6e7c2560
yeudaborodyanski <yeudaborodyanski@gmail.com>
2026-02-04 21:42:30 +0200

    NotRegistered card

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -1,10 +1,13 @@
 import {ReactNode, useEffect, useState} from "react";
 import {signIn, useSession} from "next-auth/react";
 import {remult} from "remult";
-import {User, UserRole} from "@/model/User";
-import {Card, Flex, Text} from "@tremor/react";
+import {User} from "@/model/User";
+import {Button, Card, Flex, Text} from "@tremor/react";
 import {LoadingSpinner} from "@/components/Spinner";
 import {messaging, onMessage} from "@/birebase/messaging";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import {RiRestartLine} from "@remixicon/react";
 
 const userRepo = remult.repo(User);
 
@@ -53,7 +56,32 @@ export function Auth({children}: { children: ReactNode }) {
             <LoadingSpinner/>
         </Flex>
     </Card>;
-    else return <Card className={"m-auto mt-5 w-fit"}>
-        אנא פנה למנהל המערכת על מנת לקבל גישה למערכת
+    return <NotAuthorized/>;
+}
+
+function NotAuthorized() {
+    const router = useRouter();
+
+    const goToLoginPage = () => {
+        router.push('/api/auth/signin');
+    }
+
+    return <Card className={"m-12 w-fit flex flex-col items-center bg-tremor-brand-faint"}>
+        <Image src={"/transperent-192x192.png"} alt={"Yedidim Logo"} width={180} height={180}/>
+        <Text className={"mt-4 text-2xl text-center"}>
+            ברוכים הבאים למוקדון
+        </Text>
+        <Text className={"mt-2 text-lg text-center"}>
+            נהלים והנחיות למוקדני ידידים
+        </Text>
+
+        <Text className={"mt-6 text-base text-center font-bold"}>
+            לצערנו אינך מוגדר כרגע במערכת, נא פנה למנהל המוקד על מנת להגדיר אותך.
+        </Text>
+
+        <Button icon={RiRestartLine}
+            className={"mt-6 gap-2"} onClick={goToLoginPage} variant={"light"}>
+            נסיון התחברות מחדש
+        </Button>
     </Card>;
 }
