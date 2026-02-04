c97118e935ac6b0a6f4dd5f9139f490d89c34e4f
yeudaborodyanski <yeudaborodyanski@gmail.com>
2026-02-04 21:48:43 +0200

    change firebase secrets from json to env

--- src/birebase/admin.ts
+++ src/birebase/admin.ts
@@ -1,7 +1,10 @@
 import admin from "firebase-admin";
-import settings from "firebase-service-account.json";
 
-const serviceAccount = settings as admin.ServiceAccount;
+const serviceAccount: admin.ServiceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+};
 
 if (!admin.apps.length) {
     try {
