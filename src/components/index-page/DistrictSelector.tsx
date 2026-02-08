ce3df68cb71f913a4dc5f39e081d8e5c084f1950
yeudaborodyanski <yeudaborodyanski@gmail.com>
2026-02-08 23:13:44 +0200

    split order to display options

--- src/components/index-page/DistrictSelector.tsx
+++ src/components/index-page/DistrictSelector.tsx
@@ -1,17 +1,13 @@
 import * as Tremor from "@tremor/react";
-import {Flex, Icon} from "@tremor/react";
-import {Order} from "@/utils/types";
-import {RiSortAlphabetAsc, RiSortAsc} from "@remixicon/react";
+import {Flex,} from "@tremor/react";
 import {District} from "@/model/District";
 
 export function DistrictSelector({
-    allowedDistricts, selectedDistrict, setDistrict, order, setOrder
+    allowedDistricts, selectedDistrict, setDistrict
                                  }: {
     allowedDistricts: Array<District>,
     selectedDistrict: District | "All",
     setDistrict: (d: District | "All") => void,
-    order: Order,
-    setOrder: (o: Order) => void,
 }) {
     return <Flex className={"justify-start gap-1.5 my-4"}>
         {
@@ -25,12 +21,5 @@ export function DistrictSelector({
                     key={d}>{d}</Tremor.Badge>
             })
         }
-        <Icon
-            icon={order === Order.Recent ? RiSortAlphabetAsc : RiSortAsc}
-            className={"cursor-pointer"} variant={"light"}
-            onClick={() => {
-                setOrder(order === Order.Recent ? Order.Alphabetical : Order.Recent)
-            }}
-        />
     </Flex>;
 }
