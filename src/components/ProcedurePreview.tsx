50f9b68b8f970f7431cfbfc2d0a90c35b3351176
yeudaborodyanski <yeudaborodyanski@gmail.com>
2026-02-08 23:11:38 +0200

    dense display

--- src/components/ProcedurePreview.tsx
+++ src/components/ProcedurePreview.tsx
@@ -5,15 +5,16 @@ import {plainText} from "@/utils/highlightedText";
 import {User} from "@/model/User";
 import {remult} from "remult";
 
-export function ProcedurePreview({procedure}: {
-    procedure: Procedure
+export function ProcedurePreview({procedure, dense = false}: {
+    procedure: Procedure,
+    dense?: boolean
 }) {
         return <Link href={(procedure.active || User.isAdmin(remult)) ? `/?id=${procedure.id}` : ""} passHref>
         <Card className={`p-2 cursor-pointer w-full h-full 
         ${procedure.active ? "" : "bg-primary-100 cursor-not-allowed border-dashed border-2"}
         ${procedure.type === ProcedureType.Guideline ? "bg-tremor-brand-faint border-2 border-dotted border-tremor-brand-emphasis" : ""}`}>
             <Flex flexDirection={"col"} alignItems={"start"} className={"gap-2"}>
-                <Text className={"font-bold text-xl text-start"}>
+                <Text className={`${dense ? "font-semibold text-lg" : "font-bold text-xl"} text-start`}>
                     {procedure.title}
                     {!procedure.active && <span className={"text-xs text-primary-500"}>
                         {" (לא פעיל)"}
@@ -22,7 +23,7 @@ export function ProcedurePreview({procedure}: {
                         {" (הנחיה)"}
                     </span>}
                 </Text>
-                {procedure.active && <Text className={"three-lines text-start text-xs opacity-80"}>
+                {procedure.active && !dense && <Text className={"three-lines text-start text-xs opacity-80"}>
                     {plainText(procedure.procedure)}
                 </Text>}
             </Flex>
