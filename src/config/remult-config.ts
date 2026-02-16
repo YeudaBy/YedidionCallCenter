ac5ec550b68bd2920ce1f0075e25389de9f6a2b8
yeudaborodyanski <yeudaborodyanski@gmail.com>
2026-02-16 02:18:14 +0200

    getUser on server

--- src/config/remult-config.ts
+++ src/config/remult-config.ts
@@ -2,14 +2,14 @@ import {User, UserRole} from "@/model/User";
 import {remultNext} from "remult/remult-next";
 import {Procedure} from "@/model/Procedure";
 import {Log} from "@/model/Log";
-import {ApiController} from "@/controllers/ApiController";
-import {getToken} from "next-auth/jwt";
-import {remult, SubscriptionServer} from "remult";
+import {getToken, JWT} from "next-auth/jwt";
+import {remult, UserInfo} from "remult";
 import {createPostgresDataProvider} from "remult/postgres";
 import {Category} from "@/model/Category";
 import {ProcedureCategory} from "@/model/ProcedureCategory";
 import {KnowledgeBaseController} from "@/controllers/hierarchyController";
 import {DataProviderLiveQueryStorage, SseSubscriptionServer} from "remult/server";
+import {NextApiRequest} from "next";
 
 
 const DEVELOPER_USER = {
@@ -19,25 +19,41 @@ const DEVELOPER_USER = {
     active: true,
 }
 
-
 const dataProvider = createPostgresDataProvider({
     connectionString: process.env.POSTGRES_URL,
 })
 
+async function getUser(request: NextApiRequest): Promise<UserInfo | undefined> {
+    try {
+        const token: JWT | null = await getToken({
+            req: request,
+            secret: process.env.SECRET
+        });
+
+        if (!token || !token.email) {
+            return undefined; // unauthenticated request
+        }
+
+        return User.getByEmail(remult, token.email)
+
+    } catch (error) {
+        console.error("[Remult getUser] Failed to extract user:", error);
+        return undefined;
+    }
+}
+
+const entities = [
+    User, Procedure, Log, Category, ProcedureCategory
+]
+
+const controllers = [KnowledgeBaseController]
 
 export const api = remultNext({
-    entities: [
-        User, Procedure, Log, Category, ProcedureCategory
-    ],
+    entities,
     ensureSchema: true,
-    admin: true,
-    controllers: [ApiController, KnowledgeBaseController],
-    getUser: async (req) => {
-        const jwtToken = await getToken({req})
-        // console.log("jwtToken", jwtToken)
-        if (!jwtToken?.sub) return undefined
-        return User.signIn(remult, jwtToken.sub)
-    },
+    admin: UserRole.SuperAdmin,
+    controllers,
+    getUser,
     initApi: async (api) => {
         if (await api.repo(User).count() === 0) {
             await api.repo(User).insert(DEVELOPER_USER)
